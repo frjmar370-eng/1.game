fae37e38e66e8d799607782e8edf359f7987ad18
frjmar370-eng <frjmar370@gmail.com>
2026-09-10 15:13:04 +0300

    Add one-click Unity scene and APK setup

---
+++ Assets/Editor/ShotgunProjectSetup.cs
@@ -0,0 +1,31 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class ShotgunProjectSetup
+{
+    [MenuItem("Shotgun 3D/Setup Main Scene + Android")]
+    public static void Setup()
+    {
+        System.IO.Directory.CreateDirectory("Assets/Scenes");
+        Scene scene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
+        GameObject bootstrap = new GameObject("Scene Bootstrap");
+        bootstrap.AddComponent<SceneBootstrap>();
+        EditorSceneManager.SaveScene(scene, "Assets/Scenes/Main.unity");
+        EditorBuildSettings.scenes = new[] { new EditorBuildSettingsScene("Assets/Scenes/Main.unity", true) };
+
+        PlayerSettings.productName = "Shotgun 3D";
+        PlayerSettings.companyName = "Frjmar";
+        PlayerSettings.applicationIdentifier = "com.frjmar.shotgun3d";
+        PlayerSettings.defaultScreenOrientation = ScreenOrientation.LandscapeLeft;
+        PlayerSettings.allowedAutorotateToPortrait = false;
+        PlayerSettings.allowedAutorotateToPortraitUpsideDown = false;
+        PlayerSettings.allowedAutorotateToLandscapeRight = true;
+        PlayerSettings.allowedAutorotateToLandscapeLeft = true;
+        AssetDatabase.SaveAssets();
+        Debug.Log("Shotgun 3D setup complete. Main.unity is ready.");
+    }
+}
+#endif
