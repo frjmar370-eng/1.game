1c708e6f34121a85ff23c97e4f4354f8bf40389a
frjmar370-eng <frjmar370@gmail.com>
2026-09-10 15:13:10 +0300

    Add one-click Android APK build command

---
+++ Assets/Editor/ShotgunAndroidBuild.cs
@@ -0,0 +1,28 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public static class ShotgunAndroidBuild
+{
+    [MenuItem("Shotgun 3D/Build Android APK")]
+    public static void BuildAPK()
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+        EditorUserBuildSettings.buildAppBundle = false;
+        string output = "Builds/Shotgun3D.apk";
+        System.IO.Directory.CreateDirectory("Builds");
+        BuildReport report = BuildPipeline.BuildPlayer(new BuildPlayerOptions
+        {
+            scenes = new[] { "Assets/Scenes/Main.unity" },
+            locationPathName = output,
+            target = BuildTarget.Android,
+            options = BuildOptions.None
+        });
+        if (report.summary.result == BuildResult.Succeeded)
+            Debug.Log("Shotgun 3D APK created: " + output);
+        else
+            Debug.LogError("Android APK build failed. Check the Console for the first error.");
+    }
+}
+#endif
