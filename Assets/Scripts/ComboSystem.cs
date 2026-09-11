f93af631e26424aa5376897393ef2b357836ff80
frjmar370-eng <frjmar370@gmail.com>
2026-09-11 09:57:05 +0300

    Reset project: clear repository

--- Assets/Scripts/ComboSystem.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-public class ComboSystem:MonoBehaviour{
- public static ComboSystem Instance{get;private set;}
- public int combo;float lastKill;public float window=4f;
- void Awake(){if(Instance!=null&&Instance!=this){Destroy(gameObject);return;}Instance=this;}
- public int RegisterKill(){if(Time.time-lastKill>window)combo=0;combo++;lastKill=Time.time;return combo;}
- public void Reset(){combo=0;lastKill=0;}
-}
