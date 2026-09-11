7d6f7c64a83bb0d55a856a622c9b1adb0973043d
frjmar370-eng <frjmar370@gmail.com>
2026-09-11 10:21:20 +0300

    Add jump and complete mobile control state

--- Assets/Scripts/MobileControls.cs
+++ Assets/Scripts/MobileControls.cs
@@ -1,20 +1,32 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class MobileStick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     public bool lookStick;
-    RectTransform rt; Vector2 start; int finger=-1;
-    void Awake(){rt=GetComponent<RectTransform>();}
-    public void OnPointerDown(PointerEventData e){finger=e.pointerId; RectTransformUtility.ScreenPointToLocalPointInRectangle(rt,e.position,e.pressEventCamera,out start); UpdateValue(e);}
-    public void OnDrag(PointerEventData e){if(e.pointerId==finger)UpdateValue(e);}
-    public void OnPointerUp(PointerEventData e){if(e.pointerId!=finger)return; if(lookStick)MobileControls.Look=Vector2.zero;else MobileControls.Move=Vector2.zero;finger=-1;}
-    void UpdateValue(PointerEventData e){Vector2 p;RectTransformUtility.ScreenPointToLocalPointInRectangle(rt,e.position,e.pressEventCamera,out p); Vector2 v=Vector2.ClampMagnitude((p-start)/(Mathf.Max(1,rt.rect.width*.32f)),1); if(lookStick)MobileControls.Look=v;else MobileControls.Move=v;}
+    RectTransform rt; Vector2 start; int finger = -1;
+    void Awake(){ rt = GetComponent<RectTransform>(); }
+    public void OnPointerDown(PointerEventData e){ finger=e.pointerId; RectTransformUtility.ScreenPointToLocalPointInRectangle(rt,e.position,e.pressEventCamera,out start); UpdateValue(e); }
+    public void OnDrag(PointerEventData e){ if(e.pointerId==finger) UpdateValue(e); }
+    public void OnPointerUp(PointerEventData e){ if(e.pointerId!=finger)return; if(lookStick)MobileControls.Look=Vector2.zero;else MobileControls.Move=Vector2.zero; finger=-1; }
+    void UpdateValue(PointerEventData e){ Vector2 p; RectTransformUtility.ScreenPointToLocalPointInRectangle(rt,e.position,e.pressEventCamera,out p); Vector2 v=Vector2.ClampMagnitude((p-start)/(Mathf.Max(1,rt.rect.width*.32f)),1); if(lookStick)MobileControls.Look=v;else MobileControls.Move=v; }
 }
 
 public class MobileFireButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
- public void OnPointerDown(PointerEventData e){MobileControls.Fire=true;}
- public void OnPointerUp(PointerEventData e){MobileControls.Fire=false;}
+    public void OnPointerDown(PointerEventData e){ MobileControls.Fire=true; }
+    public void OnPointerUp(PointerEventData e){ MobileControls.Fire=false; }
+}
+
+public class MobileJumpButton : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData e){ MobileControls.JumpPressed=true; }
+}
+
+public static class MobileControls
+{
+    public static Vector2 Move;
+    public static Vector2 Look;
+    public static bool Fire;
+    public static bool JumpPressed;
 }
