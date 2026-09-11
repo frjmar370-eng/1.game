bac846c5a79b5642191a1fa616bc0fe6a43784e4
frjmar370-eng <frjmar370@gmail.com>
2026-09-11 10:21:14 +0300

    Improve player movement physics and mobile response

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -1,39 +1,67 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class ThirdPersonController : MonoBehaviour
 {
-    public float moveSpeed = 5.5f;
+    public float walkSpeed = 5.5f;
+    public float sprintSpeed = 8f;
+    public float acceleration = 18f;
     public float turnSpeed = 12f;
-    public float gravity = -18f;
+    public float gravity = -24f;
+    public float jumpHeight = 1.25f;
     public Transform cameraTarget;
+
     CharacterController controller;
-    float vertical;
-    Vector2 moveInput;
-    Vector2 lookInput;
-    bool firing;
+    Vector3 planarVelocity;
+    float verticalVelocity;
     float yaw;
 
-    void Awake(){ controller = GetComponent<CharacterController>(); if(!controller) controller=gameObject.AddComponent<CharacterController>(); controller.height=3.8f; controller.radius=.55f; controller.center=new Vector3(0,1.9f,0); yaw=transform.eulerAngles.y; }
-    void Update(){
-        moveInput = MobileControls.Move;
-        lookInput = MobileControls.Look;
-        firing = MobileControls.Fire;
-        if(moveInput.sqrMagnitude>.01f){
-            Vector3 forward=Camera.main ? Camera.main.transform.forward : Vector3.forward; forward.y=0; forward.Normalize();
-            Vector3 right=Camera.main ? Camera.main.transform.right : Vector3.right; right.y=0; right.Normalize();
-            Vector3 dir=(forward*moveInput.y+right*moveInput.x); if(dir.sqrMagnitude>1)dir.Normalize();
-            controller.Move(dir*moveSpeed*Time.deltaTime);
-            Quaternion target=Quaternion.LookRotation(dir,Vector3.up); transform.rotation=Quaternion.Slerp(transform.rotation,target,turnSpeed*Time.deltaTime);
-        }
-        if(lookInput.sqrMagnitude>.001f){ yaw += lookInput.x*140f*Time.deltaTime; }
-        if(controller.isGrounded && vertical<0) vertical=-2f; vertical+=gravity*Time.deltaTime; controller.Move(Vector3.up*vertical*Time.deltaTime);
-        if(cameraTarget) cameraTarget.rotation=Quaternion.Euler(0,yaw,0);
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        controller.height = 3.8f;
+        controller.radius = .55f;
+        controller.center = new Vector3(0, 1.9f, 0);
+        controller.stepOffset = .45f;
+        controller.slopeLimit = 48f;
+        yaw = transform.eulerAngles.y;
     }
-}
 
-public static class MobileControls
-{
-    public static Vector2 Move;
-    public static Vector2 Look;
-    public static bool Fire;
+    void Update()
+    {
+        Vector2 input = MobileControls.Move;
+        Vector3 forward = Camera.main ? Camera.main.transform.forward : Vector3.forward;
+        Vector3 right = Camera.main ? Camera.main.transform.right : Vector3.right;
+        forward.y = right.y = 0f;
+        forward.Normalize(); right.Normalize();
+
+        Vector3 desired = forward * input.y + right * input.x;
+        if (desired.sqrMagnitude > 1f) desired.Normalize();
+        float targetSpeed = input.magnitude > .75f ? sprintSpeed : walkSpeed;
+        Vector3 targetVelocity = desired * targetSpeed * Mathf.Clamp01(input.magnitude);
+        planarVelocity = Vector3.MoveTowards(planarVelocity, targetVelocity, acceleration * Time.deltaTime);
+
+        if (controller.isGrounded)
+        {
+            if (verticalVelocity < 0f) verticalVelocity = -2f;
+            if (input.y > .85f && MobileControls.JumpPressed)
+                verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        verticalVelocity += gravity * Time.deltaTime;
+
+        CollisionFlags flags = controller.Move((planarVelocity + Vector3.up * verticalVelocity) * Time.deltaTime);
+        if ((flags & CollisionFlags.Above) != 0 && verticalVelocity > 0f) verticalVelocity = 0f;
+
+        if (desired.sqrMagnitude > .02f)
+        {
+            Quaternion target = Quaternion.LookRotation(desired, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, 1f - Mathf.Exp(-turnSpeed * Time.deltaTime));
+            yaw = transform.eulerAngles.y;
+        }
+
+        Vector2 look = MobileControls.Look;
+        if (look.sqrMagnitude > .001f) yaw += look.x * 150f * Time.deltaTime;
+        if (cameraTarget) cameraTarget.rotation = Quaternion.Euler(0f, yaw, 0f);
+        MobileControls.JumpPressed = false;
+    }
 }
